2a424a40dc5ed2ceb4dd0ff5b299edf33689be54
AnatolyLazarev <don.feromon@yandex.ru>
2024-03-14 05:27:51 +0300

    change src/Main

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        int initialbalance = 1500;
+        int initialbalance = 100;
         int addend = 300;
         int bonus = 0;
-        if (addend > 1000) ;
-        {
+        if (addend > 1000) {
             bonus = addend / 100;
         }
+
         int finalbalance = initialbalance + addend + bonus;
         System.out.println("Итоговый счет:" + finalbalance);
         System.out.println("Бонус: " + bonus);
+
     }
+
 }
+
